12f4686b28b690da2778f655540d357b72ed8810
Enes Coşan UZ <enescosanuz@gmail.com>
2020-10-04 23:38:10 +0300

    last changes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DiamantBOT
  
+<h2>Diamant Discord Bot</h2>
+<p>Project for learn discord.js and upgrade my javascript knowledge.</p>
+
+<p>Used:</p>
+<ul>
+<li>Discord.js</li>
+<li>fs</li>
+<li>moment</li>
+<li>quick.db</li>
+</ul>
--- index.js
+++ index.js
@@ -21,8 +21,9 @@ app.listen(3000, () => {
 
 
 // Client Settings //
-const invites = {}
 const client = new Discord.Client()
+const guildInvites = new Map();
+client.on(`inviteCreate`, async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchedInvites()))
 client.on(`ready`, () => {
     console.log(`Logged In!`)
     client.user.setPresence({
@@ -32,13 +33,11 @@ client.on(`ready`, () => {
         },
         status: 'idle'
     })
-    
-    let servers = client.guilds.cache
-    servers.forEach(g => {
-        g.fetchInvites().then(guildInvites => {
-            invites[g.id] = guildInvites;
-        })
-    })
+    client.guilds.cache.forEach(guild =>
+            guild.fetchInvites()
+                .then(invites => guildInvites.set(guild.id, invites))
+                .catch(err => console.log(err))
+    )
 });
 
 
@@ -77,7 +76,7 @@ client.on("message", async message => {
     .setColor(colours.red_dark)
     .setAuthor(message.author.username, message.author.displayAvatarURL())
     .setDescription(`Sorry ${message.author}. This command is unknown.`)
-    .setFooter(`DiamantBot | © Diamant#9542 | ${moment.utc().format(`DD MMMM HH:mm`)}`, client.user.displayAvatarURL());
+    .setFooter(`DiamantBot | © Diamant#9542 | ${moment.utc().format(`DD MMMM HH:mm`).add("3","hours")}`, client.user.displayAvatarURL());
     if(!commandfile) {message.channel.send({embed: unknown})}
 
 
@@ -85,36 +84,35 @@ client.on("message", async message => {
 
 // Invite Manager
 
-client.on('guildMemberAdd', member => {
-    if (member.guild.id === `702161798694174770`) return;
-
-    member.guild.fetchInvites().then(guildInvites => {
-        const ei = invites[member.guild.id];
-        invites[member.guild.id] = guildInvites;
-        const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-        var logChannel = member.guild.systemChannel
-        
-        if(!logChannel){
-            member.guild.owner.send(`${member.guild} is not has a system channel or specific login channel. Please make sure your server has a one of them.`)
-        } else {
-            logChannel.send(`<@${invite.inviter.id}> invite the <@${member.id}> to server. Welcome to ${member.guild.name}.`);   
+client.on('guildMemberAdd', async member => {
+    const cachedInvites = guildInvites.get(member.guild.id)
+    const newInvites = await member.guild.fetchInvites()
+    const systemChannel = db.fetch(`${member.guild.id}_systemChannel`) || member.guild.systemChannel
+    guildInvites.set(member.guild.id, newInvites)
+    try {
+        const owner = member.guild.owner
+        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses)
+        systemChannel.send(`<@${member.user.id}> joined to server. Welcome to ${member.guild.name}`)
+        if(!systemChannel) {
+            owner.send(`Your server's (${member.guild.name}) doesn't have any system channel. Please `)
         }
-        db.add(`${member.guild.id}_${invite.inviter.id}_inviteAmount`, 1)
-        db.set(`${member.guild.id}_${member.id}_inviter`, `${invite.inviter.id}`)
-    });
+        db.set(`${member.user.id}_inviteFrom`, usedInvite.inviter.id)
+        db.add(`${usedInvite.inviter.id}_inviteAmount`, 1)
+    }
+    catch(err) {
+        console.log(err)
+    }
 });
 
 client.on('guildMemberRemove', member => {
-    if (member.guild.id === `702161798694174770`) return;
-    var logChannel = member.guild.systemChannel
-    var inviter = db.get(`${member.guild.id}_${member.id}_inviter`)
-    db.add(`${member.guild.id}_${inviter}_inviteAmount`, -1)
-    db.delete(`${member.guild.id}_${member.id}_inviter`)
-
-    if(!logChannel){
-        member.guild.owner.send(`${member.guild} is not has a system channel or specific login channel. Please make sure your server has a one of them.`)
-    } else {
-        logChannel.send(`<@${member.id}> left from the server. Onu <@${inviter}> davet etmişti`)
+    const systemChannel = db.fetch(`${member.guild.id}_systemChannel`) || member.guild.systemChannel
+    const inviter = db.fetch(`${member.user.id}_inviteFrom`)
+    try {
+        systemChannel.send(`<@${member.user.id}> left from the server.`)
+        db.add(`${inviter}_inviteAmount`, -1)
+    }
+    catch(err) {
+        console.log(err)
     }
 });
 
